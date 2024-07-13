8a079d5ab6e32f42e577fe69df4da477ad164724
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-13 11:54:14 +0200

    feat: populated the comics table

--- database/migrations/2024_07_13_093213_create_comics_table.php
+++ database/migrations/2024_07_13_093213_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            // tutti gli altri campi possono essere nullable all'inizio
+            $table->string('author', 255)->nullable();
+            $table->string('publisher', 255)->nullable();
+            $table->string('language', 255)->nullable();
+            $table->tinyInteger('page_count')->nullable();
+            $table->string('genre', 255)->nullable();
+            $table->date('publication_date')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
