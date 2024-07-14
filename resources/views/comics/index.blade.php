cca3b4f6cc97d2f6f637d67a88f76f7bd3aa611d
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-14 23:25:52 +0200

    feat: add cards in comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 @section('page-title')
-    COMICS
+    COMICS-INDEX
 @endsection
 
 @section('card-comic')
-    <div class="container py-4">
+    <div class="container py-5">
+        <ul class="list-unstyled d-flex flex-wrap justify-content-between gap-5">
+            <li>
 
-        @foreach ($comics as $comic)
-            {{-- vai a comic->id --}}
-            <a href="{{ route('comics.show', $comic->id) }}">
-                <div>Titolo: {{ $comic['title'] }}</div>
-            </a>
-            <hr>
-        @endforeach
+                @foreach ($comics as $comic)
+                    {{-- vai a comic->id --}}
+                    <a href="{{ route('comics.show', $comic->id) }}">
+
+                        <img class="img-comic" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                    </a>
+                @endforeach
+            </li>
+        </ul>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,20 @@
 
 
 @section('welcome')
-    <div class="container py-3">
+    <div class="container-full py-3">
+        <img class="img-jumbo" src="{{ Vite::asset('resources/img/jumbo.jpg') }}" alt="pic">
+    </div>
+    <div class="container py-5 d-flex justify-content-center">
         <a href="{{ route('comics.create') }}">
             <div class="btn btn-primary">Crea nuovo comic</div>
         </a>
-        <ul class="list-unstyled py-3">
+        {{-- <ul class="list-unstyled py-3">
             <li>
                 @foreach ($comics as $comic)
                     <div>Titolo: {{ $comic['title'] }}</div>
                 @endforeach
             </li>
-        </ul>
+        </ul> --}}
 
 
     </div>
